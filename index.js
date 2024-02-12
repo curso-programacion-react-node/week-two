153d45c96b0426c5c12271de7ecde07a04fb65d7
Seba <sebastiancontrerasmontecinos@MacBook-Pro-de-Sebastian.local>
2024-02-11 22:26:05 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+/* Variables */
+// var nombre = "Gabriel" //No se utiliza
+let nombre = "Gabriel"; //declara una variable
+nombre = "Alejandro";
+nombre = "Adrián";
+
+const pi = 3.14; //declara una constante
+
+/* Tipos de datos */
+/* String - Cadena de texto */
+let apellido = "Salas";
+let color = 'Naranjo';
+let descripcionPlantillasLiterales = `Mi apellido es ${apellido} y mi color favorito es ${color}`
+let descripcionTradicional = "Mi apellido es" + apellido + "y mi color favorito es" + color
+
+/* console.log(apellido)
+console.log(color)
+console.log(descripcionPlantillasLiterales)
+console.log(descripcionTradicional) */
+
+/* Number - Número */
+let edad = 20
+let tiempoEnSegundos = 1.5
+let negativo = -1
+let neutro = 0
+/* console.log("suma", edad + tiempoEnSegundos)
+console.log("resta", edad - tiempoEnSegundos)
+console.log("multiplación", edad * tiempoEnSegundos)
+console.log("división", edad / tiempoEnSegundos) */
+
+/* Boolean - Toma valores verdadero o falso */
+let esRojo = false
+let esAzul = true
+
+/* Bonus */
+let nulo = null
+let indefinido = undefined
+
+/* Arrays - Listado */
+let colores = ["rojo", "azul", "verde"]
+/* console.log(colores)
+console.log(colores[0]) */
+
+/* Object - Objeto */
+let perro = {
+    edad: 2,
+    color: "negro",
+    raza: "kiltro"
+}
+/* console.log(perro)
+console.log(perro.edad)
+console.log(perro.raza) */
+
+/* Funciones Convencional */
+function hacerClick () {
+    console.log(perro)
+}
+
+/* Funciones de flecha */
+const hacerClick2 = () => console.log(perro)
+
+
+
+
